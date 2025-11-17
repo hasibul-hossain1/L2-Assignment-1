2cb52fc44444273eec6c976562b6191ee04f2dc3
shanto <hasibul.hossain.dev@gmail.com>
2025-11-17 12:05:43 +0600

    first commit

---
+++ solution.ts
@@ -0,0 +1,41 @@
+// solution 1
+
+function formatValue(params:string | number | boolean): string | number | boolean{
+    if (typeof params === "string") {
+        return params.toUpperCase();
+    }else if(typeof params === "number"){
+        return params * 10
+    }else if (typeof params === "boolean") {
+        return !params
+    }
+    throw new Error("Invalid type");
+}
+
+// solution 2
+
+function getLength(params:string | unknown[]):number {
+    if (typeof params === 'string') {
+        return params.length
+    }else if (Array.isArray(params)) {
+        return params.length
+    }
+    throw new Error("Invalid Type")
+}
+
+
+// solution 3
+
+class Person{
+    name:string;
+    age:number
+    constructor(name:string,age:number){
+        this.name=name;
+        this.age=age
+    }
+    getDetails():string{
+        return `Name: ${this.name}, Age: ${this.age}`
+    }
+}
+
+
+// solution 4
