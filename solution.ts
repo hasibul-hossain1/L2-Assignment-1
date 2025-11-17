f8572ef138b0e59c1cd958dbda5085fcb20bff5e
shanto <hasibul.hossain.dev@gmail.com>
2025-11-17 14:08:18 +0600

    Refactor functions for improved readability and consistency

--- solution.ts
+++ solution.ts
@@ -1,59 +1,73 @@
 // solution 1
 
-function formatValue(params:string | number | boolean): string | number | boolean{
-    if (typeof params === "string") {
-        return params.toUpperCase();
-    }else if(typeof params === "number"){
-        return params * 10
-    }else if (typeof params === "boolean") {
-        return !params
-    }
-    throw new Error("Invalid type");
+function formatValue(
+  params: string | number | boolean
+): string | number | boolean {
+  if (typeof params === "string") {
+    return params.toUpperCase();
+  } else if (typeof params === "number") {
+    return params * 10;
+  } else if (typeof params === "boolean") {
+    return !params;
+  }
+  throw new Error("Invalid type");
 }
 
 // solution 2
 
-function getLength(params:string | unknown[]):number {
-    if (typeof params === 'string') {
-        return params.length
-    }else if (Array.isArray(params)) {
-        return params.length
-    }
-    throw new Error("Invalid Type")
+function getLength(params: string | unknown[]): number {
+  if (typeof params === "string") {
+    return params.length;
+  } else if (Array.isArray(params)) {
+    return params.length;
+  }
+  throw new Error("Invalid Type");
 }
-
 
 // solution 3
 
-class Person{
-    name:string;
-    age:number
-    constructor(name:string,age:number){
-        this.name=name;
-        this.age=age
-    }
-    getDetails():string{
-        return `Name: ${this.name}, Age: ${this.age}`
-    }
+class Person {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  getDetails(): string {
+    return `Name: ${this.name}, Age: ${this.age}`;
+  }
 }
-
 
 // solution 4
 
-function filterByRating(params:Array<{title:string,rating:number}>) :Array<{title:string,rating:number}>|[] {
-    return params.filter((item)=>item.rating>=4)
+function filterByRating(
+  params: Array<{ title: string; rating: number }>
+): Array<{ title: string; rating: number }> | [] {
+  return params.filter((item) => item.rating >= 4);
 }
 
 // solution 5
-type UserType={
-    id:number;
-    name:string;
-    email:string;
-    isActive:boolean
-}
+type UserType = {
+  id: number;
+  name: string;
+  email: string;
+  isActive: boolean;
+};
 
-function filterActiveUsers(params:Array<UserType>):Array<UserType>|[] {
-    return params.filter(user=>user.isActive)
+function filterActiveUsers(params: Array<UserType>): Array<UserType> | [] {
+  return params.filter((user) => user.isActive);
 }
 
 //solution 6
+
+interface Book {
+  title: string;
+  author: string;
+  publishedYear:number;
+  isAvailable:boolean;
+}
+
+function printBookDetails(book:Book) {
+    console.log(`Title: ${book.title}, Author: ${book.author}, Published: ${book.publishedYear}, Available: ${book.isAvailable?"Yes":"No"}`);
+}
+
